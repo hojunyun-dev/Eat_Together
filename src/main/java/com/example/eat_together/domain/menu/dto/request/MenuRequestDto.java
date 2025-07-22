a56e877ab8f69ee32d0972831f86718a9442c413
4ouChan <kumho3983398@naver.com>
2025-07-22 16:53:29 +0900

    refact : CreateMenuRequestDto -> MenuRequestDto로 변경

--- src/main/java/com/example/eat_together/domain/menu/dto/request/MenuRequestDto.java
+++ src/main/java/com/example/eat_together/domain/menu/dto/request/MenuRequestDto.java
@@ -3,7 +3,7 @@ package com.example.eat_together.domain.menu.dto.request;
 import lombok.Getter;
 
 @Getter
-public class CreateMenuRequestDto {
+public class MenuRequestDto {
 
     private final String imageUrl;
 
@@ -13,7 +13,7 @@ public class CreateMenuRequestDto {
 
     private final String description;
 
-    public CreateMenuRequestDto(String imageUrl, String name, double price, String description) {
+    public MenuRequestDto(String imageUrl, String name, double price, String description) {
         this.imageUrl = imageUrl;
         this.name = name;
         this.price = price;
