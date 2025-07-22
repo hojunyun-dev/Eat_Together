919603ffd91603a27259529abc22c7a49bd1540e
4ouChan <kumho3983398@naver.com>
2025-07-22 16:34:43 +0900

    refact : Response 메시지 ENUM 활용 구조로 변경

--- src/main/java/com/example/eat_together/domain/menu/controller/MenuController.java
+++ src/main/java/com/example/eat_together/domain/menu/controller/MenuController.java
@@ -3,6 +3,7 @@ package com.example.eat_together.domain.menu.controller;
 import com.example.eat_together.domain.menu.dto.request.CreateMenuRequestDto;
 import com.example.eat_together.domain.menu.dto.respones.PagingMenuResponseDto;
 import com.example.eat_together.domain.menu.entity.Menu;
+import com.example.eat_together.domain.menu.message.ResponseMessage;
 import com.example.eat_together.domain.menu.service.MenuService;
 import com.example.eat_together.global.dto.ApiResponse;
 import org.springframework.data.domain.Pageable;
@@ -26,17 +27,17 @@ public class MenuController {
 
         menuService.createMenu(storeId, requestDto);
 
-        ApiResponse<Menu> response = new ApiResponse<>("메뉴 등록이 완료되었습니다.", null);
+        ApiResponse<Menu> response = new ApiResponse<>(ResponseMessage.MENU_CREATED_SUCCESS.getMessage(), null);
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<ApiResponse<PagingMenuResponseDto>> getMenuByStore(@PathVariable Long storeId, @PageableDefault Pageable pageable) {
+    public ResponseEntity<ApiResponse<PagingMenuResponseDto>> getMenusByStore(@PathVariable Long storeId, @PageableDefault Pageable pageable) {
 
         PagingMenuResponseDto menusByStoreId = menuService.getMenusByStoreId(storeId, pageable);
 
-        ApiResponse<PagingMenuResponseDto> response = new ApiResponse<>("메뉴 목록 조회가 완료되었습니다.", menusByStoreId);
+        ApiResponse<PagingMenuResponseDto> response = new ApiResponse<>(ResponseMessage.MENU_LIST_FETCH_SUCCESS.getMessage(), menusByStoreId);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
---
+++ src/main/java/com/example/eat_together/domain/menu/message/ResponseMessage.java
@@ -0,0 +1,18 @@
+package com.example.eat_together.domain.menu.message;
+
+import lombok.Getter;
+
+@Getter
+public enum ResponseMessage {
+
+
+    MENU_CREATED_SUCCESS("메뉴 등록이 완료되었습니다."),
+    MENU_LIST_FETCH_SUCCESS("메뉴 목록 조회가 완료되었습니다.");
+
+
+    private final String message;
+
+    ResponseMessage(String message) {
+        this.message = message;
+    }
+}
