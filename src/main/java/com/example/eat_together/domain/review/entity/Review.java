0652300b0b117a66da47371d67ee3745dc0e7d48
KimDohwan24 <94787456+KimDohwan24@users.noreply.github.com>
2025-07-23 20:45:08 +0900

    Feat : 리뷰 엔티티 추가

---
+++ src/main/java/com/example/eat_together/domain/review/entity/Review.java
@@ -0,0 +1,32 @@
+package com.example.eat_together.domain.review.entity;
+
+import com.example.eat_together.domain.order.entity.Order;
+import com.example.eat_together.domain.user.entity.User;
+import com.example.eat_together.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Review extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "rating")
+    private String rating;
+}
