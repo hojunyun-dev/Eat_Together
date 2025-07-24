32d3a83f6cacccbe80020897772e8a56030a3813
seoyeon <dlatjdus0303@naver.com>
2025-07-24 21:11:24 +0900

    refactor: 주문 상태 변경 요청 파라미터를 OrderStatus에서 OrderStatusUpdateRequestDto로 변경

---
+++ src/main/java/com/example/eat_together/domain/order/dto/request/OrderStatusUpdateRequestDto.java
@@ -0,0 +1,13 @@
+package com.example.eat_together.domain.order.dto.request;
+
+import com.example.eat_together.domain.order.orderEnum.OrderStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class OrderStatusUpdateRequestDto {
+
+    @NotNull(message = "주문 상태를 입력해주세요.")
+    private OrderStatus status;
+
+}
