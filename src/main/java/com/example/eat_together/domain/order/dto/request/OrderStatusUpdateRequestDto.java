eaaa66925d38f7549f1b663c7e05f37f91581627
seoyeon <dlatjdus0303@naver.com>
2025-07-29 17:59:52 +0900

    feat: @NoArgsConstructor 추가

--- src/main/java/com/example/eat_together/domain/order/dto/request/OrderStatusUpdateRequestDto.java
+++ src/main/java/com/example/eat_together/domain/order/dto/request/OrderStatusUpdateRequestDto.java
@@ -3,8 +3,10 @@ package com.example.eat_together.domain.order.dto.request;
 import com.example.eat_together.domain.order.orderEnum.OrderStatus;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class OrderStatusUpdateRequestDto {
 
     @NotNull(message = "주문 상태를 입력해주세요.")
