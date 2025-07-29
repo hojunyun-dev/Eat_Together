07bb2449acfd8ccf62acf82b0113036ab49a5f17
4ouChan <kumho3983398@naver.com>
2025-07-29 16:49:57 +0900

    feat : 카테고리별 매장 목록 조회 기능, 매장 단건 조회 기능 Redis 적용

--- src/main/java/com/example/eat_together/domain/store/dto/response/PagingStoreResponseDto.java
+++ src/main/java/com/example/eat_together/domain/store/dto/response/PagingStoreResponseDto.java
@@ -1,14 +1,18 @@
 package com.example.eat_together.domain.store.dto.response;
 
 import com.example.eat_together.domain.store.entity.Store;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PagingStoreResponseDto {
 
     List<StoreResponseDto> storeList;
