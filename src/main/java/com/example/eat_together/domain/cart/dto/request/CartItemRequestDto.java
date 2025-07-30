08e981dce02dae5540a4afdedf4b134ead5c985e
Gyum1 <xoruadl123@gmail.com>
2025-07-30 15:30:09 +0900

    [fix] Cart 도메인 JSON 직렬화 문제 해결을 위해 RequestDto에 @NoArgsConstructor 추가

--- src/main/java/com/example/eat_together/domain/cart/dto/request/CartItemRequestDto.java
+++ src/main/java/com/example/eat_together/domain/cart/dto/request/CartItemRequestDto.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
@@ -11,6 +12,7 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@NoArgsConstructor
 public class CartItemRequestDto {
 
     /**
